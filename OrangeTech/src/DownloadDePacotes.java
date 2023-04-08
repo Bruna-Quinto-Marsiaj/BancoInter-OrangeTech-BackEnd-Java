40a5327a0ad1c0280c68a5dd340d82a27253a5a1
Bruna Quinto Marsiaj <110314990+Bruna-Quinto-Marsiaj@users.noreply.github.com>
2023-04-07 23:47:31 -0300

    Exercicio Leitura da Gertrudes

--- OrangeTech/src/DownloadDePacotes.java
+++ OrangeTech/src/DownloadDePacotes.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
             int tamanho = leitor.nextInt();
 
             // TODO: Crie a condição necessária para que, de acordo com o tamanho, seja printado no console barras representando o download
-            
+
                 for (int i = 1; i <= tamanho; i++){
                     System.out.print("/");
                 }
